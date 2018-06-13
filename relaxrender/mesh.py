b2c1e0456fe19d83e715b75ef9902e82724c7ea8
RenShuhuai-Andy <739584819@qq.com>
2018-06-13 23:00:12 +0800

    添加了第一版代码

--- relaxrender/mesh.py
+++ relaxrender/mesh.py
@@ -5,6 +5,7 @@ from .triangle import Triangles
 
 __all__ = ['Mesh']
 
+
 class Mesh:
     def __init__(self, triangles=None, texs=None, tex_pos=None):
         self.triangles = triangles
@@ -13,4 +14,3 @@ class Mesh:
 
     def verify(self):
         pass
-        
