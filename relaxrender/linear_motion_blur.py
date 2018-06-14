af9887cabfdb5e6b3472bc1a09f5488fcbf9c2ae
RenShuhuai-Andy <739584819@qq.com>
2018-06-14 16:10:11 +0800

    添加了对指定矩形域进行模糊的功能

--- relaxrender/linear_motion_blur.py
+++ relaxrender/linear_motion_blur.py
@@ -3,10 +3,9 @@ import numpy as np
 from PIL import Image
 from scipy.signal import convolve2d
 
-from relaxrender.LineDictionary import LineDictionary  # 导入角度-？字典
+from relaxrender.LineDictionary import LineDictionary  # 导入{角度-核矩阵}的字典
 
-lineLengths = [3, 5, 7, 9, 20]  # 线性长度
-lineTypes = ["full", "right", "left"]  # 控制模糊内核是全部应用还是仅应用于其左/右一半。
+lineLengths = [5, 10, 20]  # 线性长度
 
 lineDict = LineDictionary()
 
@@ -16,40 +15,47 @@ def LinearMotionBlur_random(img):
     随机选择模式进行模糊
     """
     lineLengthIdx = np.random.randint(0, len(lineLengths))
-    lineTypeIdx = np.random.randint(0, len(lineTypes))
     lineLength = lineLengths[lineLengthIdx]
-    lineType = lineTypes[lineTypeIdx]
     lineAngle = randomAngle(lineLength)
-    return LinearMotionBlur(img, lineLength, lineAngle, lineType)
+    return LinearMotionBlur(img, lineLength, lineAngle)
 
 
-def LinearMotionBlur(img, dim, angle, linetype):
+def LinearMotionBlur(img, dim, angle, area=None):
+    """
+    依据参数进行运动模糊
+    img为由PIL库导入的图片，dim为卷积核的宽度，angle为模糊方向，area为矩形模糊区域(包含四个元素的元组)
+    """
     imgarray = np.array(img, dtype="float32")  # 将图片转换为数组
-    kernel = LineKernel(dim, angle, linetype)  # 获得线性核
-    imgs = []
+    imgshape = imgarray.shape
+    kernel = LineKernel(dim, angle)  # 获得线性核
+    if area is None:  # 若不传area参数，则默认对整张图片进行模糊
+        r1, r2, c1, c2 = (0, imgshape[0], 0, imgshape[1])  # r1(row1)是矩形上边界，c1(colum1)是矩形左边界，r2是矩形下边界,c2是矩形右边界
+    else:
+        r1, r2, c1, c2 = area
     for d in range(3):
         # 对图像的RGB三个通道做循环处理
-        img_conv_d = convolve2d(imgarray[:, :, d], kernel, mode='same', boundary='symm')
-        imgs.append(img_conv_d)
-    img_conv = np.stack(imgs, axis=2).astype("uint8")
-    img = Image.fromarray(img_conv)  # 将数组转换回图片
+        # 只对area圈出的区域进行模糊，ing_conv_d的维度是(r2-r1)*(c2-c1)
+        img_conv_d = convolve2d(imgarray[r1:r2, c1:c2, d], kernel, mode='same', boundary='symm')
+        imgarray[r1:r2, c1:c2, d] = img_conv_d  # 将区域中的原像素值替换为卷积后的像素值
+    imgarray = imgarray.astype("uint8")
+    img = Image.fromarray(imgarray)  # 将数组转换回图片
     return img
 
 
-def LineKernel(dim, angle, linetype):
+def LineKernel(dim, angle):
     """
     线性核
     """
     kernelwidth = dim  # 核的宽度
-    kernelCenter = int(math.floor(dim / 2))  # 核的中心
-    angle = SanitizeAngleValue(kernelCenter, angle)  # 运动线的角度。将被定位到与给定内核大小相关的最近的一个。
-    kernel=lineDict.Createkernel(dim,angle)
+    kernelCenter = int(math.floor(kernelwidth / 2))  # 核的中心
+    angle = sanitize_angleValue(kernelCenter, angle)  # 运动线的角度。将被定位到与给定内核大小相关的最近的一个。
+    kernel = lineDict.create_kernel(kernelwidth, angle)
     normalizationFactor = np.count_nonzero(kernel)  # 返回kernel中非0元素的个数
     kernel = kernel / normalizationFactor  # 归一化
     return kernel
 
 
-def SanitizeAngleValue(kernelCenter, angle):
+def sanitize_angleValue(kernelCenter, angle):
     """
     输入角度规范化
     """
--- tests/test_motionblur.py
+++ tests/test_motionblur.py
@@ -6,11 +6,13 @@ import numpy as np
 import math
 
 img = Image.open('./picture_test.jpg')  # shape of img is (682,1023,3)
-img_blur = linear_motion_blur.LinearMotionBlur(img, dim=10, angle=45, linetype="full")
-# img_blur = linear_motion_blur.LinearMotionBlur_random(img)
+area = (40, 681, 217, 766)
+# img_origin = linear_motion_blur.LinearMotionBlur_random(img)
+img_blur = linear_motion_blur.LinearMotionBlur(img, dim=20, angle=135, area=area)
+# img_blur_withoutarea = linear_motion_blur.LinearMotionBlur(img, dim=20, angle=135)
 plt.figure("img_blur")  # 图像窗口名称
+# plt.imshow(img_origin)
 plt.imshow(img_blur)
 plt.axis('on')  # 开启坐标轴
 plt.title('img_blur')  # 图像题目
 plt.show()
-
