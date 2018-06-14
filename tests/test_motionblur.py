8f1af6091547d47375411151854771ed605b6f6f
liujunao <1713507920@qq.com>
2018-06-14 23:49:50 +0800

    功能的进一步优化

--- tests/test_motionblur.py
+++ tests/test_motionblur.py
@@ -1,14 +1,12 @@
 from relaxrender import linear_motion_blur
 from matplotlib import pyplot as plt
 import imageio
-from PIL import Image
 import numpy as np
-import math
 
-img = Image.open('./picture_test.jpg')  # shape of img is (682,1023,3)
+img = imageio.imread('./picture_test.jpg')  # shape of img is (682,1023,3)
 area = (40, 681, 217, 766)
 # img_origin = linear_motion_blur.LinearMotionBlur_random(img)
-img_blur = linear_motion_blur.LinearMotionBlur(img, dim=20, angle=135, area=area)
+img_blur = linear_motion_blur.LinearMotionBlur(img, dim=20, angle=135, area=area, filename='picture_test_blur.jpg')
 # img_blur_withoutarea = linear_motion_blur.LinearMotionBlur(img, dim=20, angle=135)
 plt.figure("img_blur")  # 图像窗口名称
 # plt.imshow(img_origin)
